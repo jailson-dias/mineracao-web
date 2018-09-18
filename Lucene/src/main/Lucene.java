302b57a62ebad52506ce473b3d144db72e2be13d
Jailson Dias <jcd2@cin.ufpe.br>
2018-09-18 17:08:27 -0300

    codigo lucene

---
+++ Lucene/src/main/Lucene.java
@@ -0,0 +1,86 @@
+package main;
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.br.BrazilianAnalyzer;
+import org.apache.lucene.analysis.miscellaneous.StemmerOverrideFilter;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
+public class Lucene {
+	public static void main(String [] args) {
+		StandardAnalyzer analyzer = new StandardAnalyzer();
+		
+		Directory index = new RAMDirectory();
+
+		IndexWriterConfig config = new IndexWriterConfig(analyzer);
+
+		IndexWriter w;
+		try {
+			w = new IndexWriter(index, config);
+			addDoc(w, "Lucene in Action", "193398817");
+			addDoc(w, "Lucene for Dummies", "55320055Z");
+			addDoc(w, "Managing Gigabytes", "55063554A");
+			addDoc(w, "The Art of Computer Science", "9900333X");
+			w.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String querystr = args.length > 0 ? args[0] : "lucene";
+		try {
+			Query q = new QueryParser("title", analyzer).parse(querystr);
+			
+			int hitsPerPage = 10;
+			IndexReader reader = DirectoryReader.open(index);
+			IndexSearcher searcher = new IndexSearcher(reader);
+			TopDocs docs = searcher.search(q, hitsPerPage);
+			ScoreDoc[] hits = docs.scoreDocs;
+			
+			System.out.println("Found " + hits.length + " hits.");
+			for(int i=0;i<hits.length;++i) {
+			    int docId = hits[i].doc;
+			    Document d = searcher.doc(docId);
+			    System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+			}
+			
+//			BrazilianAnalyzer.getDefaultStopSet()
+			CharArraySet stopWords = BrazilianAnalyzer.getDefaultStopSet();
+			String a = "meu nome é jailson";
+//			a.split(" ").;
+//			new StemmerOverrideFilter.Builder() 
+			
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+		  Document doc = new Document();
+		  doc.add(new TextField("title", title, Field.Store.YES));
+		  doc.add(new StringField("isbn", isbn, Field.Store.YES));
+		  w.addDocument(doc);
+		}
+}
